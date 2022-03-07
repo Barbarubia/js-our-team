02e6a906c31cd9a375e296fa6abd8868700a3952
Barbarubia <dnl.desimone@gmail.com>
2022-03-07 11:29:02 -0300

    terminato ciclo che genera la card dei membri

--- js/script.js
+++ js/script.js
@@ -37,13 +37,26 @@ const eleTeamContainer = document.querySelector('.team-container');
 
 // Ciclo for che crea una card per ogni membro del team
 for (let i = 0; i < arrMembersTeam.length; i++) {
+    // generazione card per ogni membro
     let eleTeamCard = document.createElement('div');
     eleTeamCard.classList.add('team-card');
     eleTeamContainer.append(eleTeamCard);
+    // generazione elemento che contiene l'immagine del membro pescandola dall'array dei membri
     let eleCardImage = document.createElement('div');
     eleCardImage.classList.add('card-image');
     eleTeamCard.append(eleCardImage);
     let eleImg = document.createElement('img')
     eleImg.src = `img/${arrMembersTeam[i].image}`;
+    eleImg.alt = `img/${arrMembersTeam[i].name}`;
     eleCardImage.append(eleImg);
+    // generazione elemento che contiene nome e ruolo del membro pescandoli dall'array dei membri
+    let eleCardText = document.createElement('div');
+    eleCardText.classList.add('card-text');
+    eleTeamCard.append(eleCardText);
+    let eleName = document.createElement('h3');
+    eleName.innerHTML = `${arrMembersTeam[i].name}`;
+    eleCardText.append(eleName);
+    let eleRole = document.createElement('p');
+    eleRole.innerHTML = `${arrMembersTeam[i].role}`;
+    eleCardText.append(eleRole);
 }
